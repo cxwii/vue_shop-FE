16f84a1031862b182371f411370385cd6f289e1b
cxwii <1471749154@qq.com>
2020-11-20 19:59:33 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+// 导入的弹框提示(上面的Message,这里不知道为什么不能用下面这种方式了)
+// import { Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+// 挂载的弹框提示
+Vue.prototype.$message = Message
